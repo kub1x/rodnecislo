17be44f3ce15764d3894a41c34328d0638f8348e
Jakub kub1x Podlaha <j.podlaha@gmail.com>
2017-06-08 12:17:59 +0200

    build(typescript): Add types definition

---
+++ lib/rodnecislo.d.ts
@@ -0,0 +1,24 @@
+
+declare function rodnecislo(rc: string): rodnecislo.RodneCislo;
+
+declare namespace rodnecislo {
+  interface RodneCislo extends Object {
+    isValid(): boolean
+
+    isMale(): boolean
+    isFemale(): boolean
+
+    year(): number
+    month(): number
+    day(): number
+
+    birthDate(): Date
+    birthDateAsString(): string
+
+    isAdult(adulthood?: number): boolean
+    age(): number
+
+    toDIC(): string
+  }
+}
+
